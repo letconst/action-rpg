16425fe7efbb76db5d88e768464d5c30580c568a
letconst <2000takeru93@gmail.com>
2021-10-23 15:34:17 +0900

    feat: Actorのモーション確認機能を実装

---
+++ Assets/Sctipts/MotionEvent/MotionListItem.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MotionListItem : MonoBehaviour
+{
+    [SerializeField]
+    private Button listItemButton;
+
+    [SerializeField]
+    private Text motionNameLabel;
+
+    /// <summary>
+    /// モーションボタンを初期化する
+    /// </summary>
+    /// <param name="motionName">表示名</param>
+    /// <param name="onClick">クリック時の処理</param>
+    public void Setup(string motionName, System.Action onClick)
+    {
+        listItemButton.onClick.AddListener(() => onClick?.Invoke());
+        motionNameLabel.text = motionName;
+    }
+}
